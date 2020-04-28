e74251418f6ac56c73a63b3c1c58fa2fb69596dc
Michael Smith <michael_smith17@hotmail.co.uk>
2020-04-28 14:40:09 +0100

    heroku changes

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,15 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 let dbUrl;
-if(process.env.NODE_ENV === "test") dbUrl = process.env.DB_CONNECTION_TEST
-else if(process.env.NODE_ENV === "dev") dbUrl = process.env.DB_CONNECTION_DEV
-else if(process.env.NODE_ENV === "prod") dbUrl = process.env.DB_CONNECTION_PROD
+if (process.env.NODE_ENV === 'test') dbUrl = process.env.DB_CONNECTION_TEST;
+else if (process.env.NODE_ENV === 'dev') dbUrl = process.env.DB_CONNECTION_DEV;
+else if (process.env.NODE_ENV === 'prod')
+  dbUrl = process.env.DB_CONNECTION_PROD;
 
+console.log(dbUrl);
 mongoose
   .connect(dbUrl, {
     useNewUrlParser: true,
   })
-  .then(() => console.log("connected"))
+  .then(() => console.log('connected'))
   .catch((err) => console.log(err));
