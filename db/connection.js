0cb836e98b0137a584c93dd7c1b0cf7e24a08098
Michael Smith <michael_smith17@hotmail.co.uk>
2020-04-28 14:56:45 +0100

    heroku changes

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 let dbUrl;
 if (process.env.NODE_ENV === 'test') dbUrl = process.env.DB_CONNECTION_TEST;
 else if (process.env.NODE_ENV === 'dev') dbUrl = process.env.DB_CONNECTION_DEV;
-else if (process.env.NODE_ENV === 'prod')
+else if (process.env.NODE_ENV === 'production')
   dbUrl = process.env.DB_CONNECTION_PROD;
 
 console.log(dbUrl);
