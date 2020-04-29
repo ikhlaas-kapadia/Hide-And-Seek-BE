76297b022ca940646b077fc15973e719fac13eba
Ikhlaas Kapadia <ikhlaas.ka@gmail.com>
2020-04-29 10:39:02 +0100

    add cors package

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
 require("dotenv").config();
 require("./db/connection");
 
 const { apiRouter } = require("./routers/api-router");
 const { handleErrors } = require("./errors/errors");
 
+app.use(cors());
+
 app.use(express.json());
+
 app.use("/api", apiRouter);
+
 app.use(handleErrors);
 
 module.exports = { app };
