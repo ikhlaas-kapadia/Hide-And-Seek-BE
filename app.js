19e481f734459612135c049c0393094da855b9de
Michael Smith <michael_smith17@hotmail.co.uk>
2020-04-29 10:42:54 +0100

    changes

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-require("dotenv").config();
-require("./db/connection");
+require('dotenv').config();
+require('./db/connection');
 
-const { apiRouter } = require("./routers/api-router");
-const { handleErrors } = require("./errors/errors");
+const { apiRouter } = require('./routers/api-router');
+const { handleErrors } = require('./errors/errors');
 
 app.use(express.json());
-app.use("/api", apiRouter);
+app.use('/api', apiRouter);
 app.use(handleErrors);
 
 module.exports = { app };
